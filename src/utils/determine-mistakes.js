5d4714ec3e073fd2b41f230f90b366adda56401b
OluC94 <coker.olu94@gmail.com>
2024-06-26 14:27:49 +0100

    update mistake counter

--- src/utils/determine-mistakes.js
+++ src/utils/determine-mistakes.js
@@ -6,14 +6,9 @@
  */
 export default function determineMistakes(guessedLetters, targetWord) {
     // return the number of guessed letters that are not in the target word
-    let mistakeCount = 0;
-
-    for (const letter of guessedLetters) {
-        const letterIsMistake = !targetWord.includes(letter);
-        if (letterIsMistake) {
-            mistakeCount++;
-        }
+    function isNotInTargetWord(letter){
+        return !targetWord.includes(letter);
     }
-
-    return mistakeCount;
+    
+    return guessedLetters.filter(isNotInTargetWord).length;
 }
